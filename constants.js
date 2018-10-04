2ebf5b43eebb0366d28659b4ae912c1389bdb359
Acidiney Dias <acidineydias@gmail.com>
2018-10-04 17:16:22 +0100

    Refectory github urls and add a github test file with jest

---
+++ constants.js
@@ -0,0 +1,10 @@
+// Github API
+const apiUrl = 'https://api.github.com/';
+
+// repository stuture url default
+const reposConfig = 'repos/braziljs/weekly/issues';
+
+module.exports = {
+  apiUrl,
+  reposConfig
+}
