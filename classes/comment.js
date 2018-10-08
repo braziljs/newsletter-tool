487ef99e3081f5d07ae14a2efada215d395ea66c
Renato Siqueira <renatoelysiqueira@gmail.com>
2018-10-08 11:02:29 -0300

    Issue #5/ #1

--- classes/comment.js
+++ classes/comment.js
@@ -1,47 +1,46 @@
-
 class Comment {
-    constructor (data = {}) {
-        this.applyData(data)
+  constructor(data = {}) {
+    this.applyData(data)
+  }
+
+  applyData(data) {
+    this.mdBody = data.body
+    this.htmlBody = window.markdown.toHTML(data.body)
+    this.title = this.mdBody.match(/^[\*_][\*_]\[(.+)\].+[\*_][\*_][\n\r]/)
+    this.title = this.title ? this.title[1] : ''
+    this.link = this.mdBody.match(/^[\*_][\*_].+\((.+)\)[\*_][\*_][\n\r]/)
+    this.link = this.link ? this.link[1] : ''
+    this.outline = `<a href="${this.link}">${this.title}</a>:<br/>${this.link}<br/>`
+    this.created_at = data.created_at
+    this.id = data.id
+    this.url = data.url
+    this.author = {
+      id: data.user.id,
+      login: data.user.login,
+      avatar: data.user.avatar_url,
+      url: data.user.html_url
+    }
+    this.applyCategory()
+  }
+
+  applyCategory() {
+    let cat = this.mdBody.match(/[\n\r][\*_](.+)[\*_]$/m)
+    let testCat = cat[1].toLowerCase()
+
+    if (testCat.match(/(new|not.cia|novidade|an.nci|announc)/)) {
+      cat = 'Notícias'
+    } else if (testCat.match(/(event|confer.ncia|summit|workshop|congresso)/)) {
+      cat = 'Eventos'
+    } else if (testCat.match(/(artigo|article|text|post)/)) {
+      cat = 'Artigos'
+    } else if (testCat.match(/(talk|palestr|aula|curso|v.deo)/)) {
+      cat = 'Vídeos'
+    } else if (testCat.match(/(tool|demo|experiment|ferrament)/)) {
+      cat = 'Demos e Ferramentas'
+    } else {
+      cat = 'Outros'
     }
 
-    applyData (data) {
-        this.mdBody = data.body
-        this.htmlBody = window.markdown.toHTML(data.body)
-        this.title = this.mdBody.match(/^[\*_][\*_]\[(.+)\].+[\*_][\*_][\n\r]/)
-        this.title = this.title ? this.title[1] : ''
-        this.link = this.mdBody.match(/^[\*_][\*_].+\((.+)\)[\*_][\*_][\n\r]/)
-        this.link = this.link ? this.link[1] : ''
-        this.outline = `<a href="${this.link}">${this.title}</a>:<br/>${this.link}<br/>`
-        this.created_at = data.created_at
-        this.id = data.id
-        this.url = data.url
-        this.author = {
-            id: data.user.id,
-            login: data.user.login,
-            avatar: data.user.avatar_url,
-            url: data.user.html_url
-        }
-        this.applyCategory()
-    }
-
-    applyCategory () {
-        let cat = this.mdBody.match(/[\n\r][\*_](.+)[\*_]$/m)
-        let testCat = cat[1].toLowerCase()
-
-        if (testCat.match(/(new|not.cia|novidade|an.nci|announc)/)) {
-            cat = "Notícias"
-        } else if (testCat.match(/(event|confer.ncia|summit|workshop|congresso)/)) {
-            cat = "Eventos"
-        } else if (testCat.match(/(artigo|article|text|post)/)) {
-            cat = "Artigos"
-        } else if (testCat.match(/(talk|palestr|aula|curso|v.deo)/)) {
-            cat = "Vídeos"
-        } else if (testCat.match(/(tool|demo|experiment|ferrament)/)) {
-            cat = "Demos e Ferramentas"
-        } else {
-            cat = 'Outros'
-        }
-        
-        this.category = cat
-    }
+    this.category = cat
+  }
 }
